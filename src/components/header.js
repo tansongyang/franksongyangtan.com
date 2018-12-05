c2fac8943884031d0fe541a63519f3d6189b768f
Frank Tan <tansongyang@gmail.com>
2018-12-05 17:11:32 -0500

    Use CSS Modules in header
    At this point, I am more productive with CSS Modules, but hopefully
    there will be a chance some day to experiment more in this repo.

--- src/components/header.js
+++ src/components/header.js
@@ -2,28 +2,13 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import styles from './header.module.css'
+
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'black',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+  <div className={styles.header}>
+    <div className={styles.innerWrapper}>
+      <h1 className={styles.h1}>
+        <Link to="/" className={styles.link}>
           {siteTitle}
         </Link>
       </h1>
