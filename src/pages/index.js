4cedd3f7981975aa10d22f8e2e74d562ac357516
Frank Tan <tansongyang@gmail.com>
2018-12-09 14:56:17 -0500

    Add contact details to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Link from '../components/link'
 
 export default function IndexPage() {
   return (
     <Layout>
       <h1>Hello!</h1>
-      <p>Welcome to my personal site.</p>
       <p>
-        I'm still working on it, but hopefully there will be interesting things
-        here soon!
+        My name is Frank Songyang Tan. On the internet, my username is usually
+        my Chinese name, <code>tansongyang</code>. In Chinese, that's 谭松阳.
+      </p>
+      <h2>Contact Me</h2>
+      <p>
+        If you want to get in contact with me, I can be reached via{' '}
+        <Link to="https://twitter.com/tansongyang">Twitter direct message</Link>
+        .
       </p>
     </Layout>
   )
