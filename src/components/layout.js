93c9ded2e4a8015553a9b2e7387cd98786e5a84e
Frank Tan <tansongyang@gmail.com>
2018-12-31 21:55:59 -0500

    Make everything narrower

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,7 +35,7 @@ const Layout = ({ children }) => (
         <main
           style={{
             margin: '0 auto',
-            maxWidth: 960,
+            maxWidth: '40rem',
             padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
           }}
