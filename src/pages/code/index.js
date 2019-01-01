2db19e65644062d06783c42b8c986be69b3d334c
Frank Tan <tansongyang@gmail.com>
2018-12-31 19:57:38 -0500

    Fix incorrect use of `class` attribute

--- src/pages/code/index.js
+++ src/pages/code/index.js
@@ -28,7 +28,7 @@ export default function CodeIndexPage() {
   return (
     <Layout>
       <Helmet title="Code" />
-      <h1 class="screen-reader-only">Code</h1>
+      <h1 className="screen-reader-only">Code</h1>
       <p>
         I'm a front-end software engineer with .NET experience. By day, I work
         with <Link to="https://www.emberjs.com/">Ember.js</Link> and{' '}
