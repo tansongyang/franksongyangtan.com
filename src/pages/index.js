565f0382bab71e2f0936f7b16b924a5dc0c170e3
Frank Tan <tansongyang@gmail.com>
2019-02-07 21:04:01 -0500

    Add photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,18 @@ export default function IndexPage() {
   return (
     <Layout>
       <h1 className="screen-reader-only">Frank Tan (tansongyang)</h1>
+      <img
+        style={{
+          display: 'block',
+          width: '160px',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+          borderRadius: '50%',
+        }}
+        className="with-spacing"
+        src="https://pbs.twimg.com/profile_images/985307670549692416/j9DaOXnR_400x400.jpg"
+        alt="Photo of Frank Tan (tansongyang)"
+      />
       <p>
         My name is Frank Tan. On the internet, my username is usually my Chinese
         name, <code>tansongyang</code>. In Chinese, that's 谭松阳.
